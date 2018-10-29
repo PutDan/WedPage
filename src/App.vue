ddecb8e00541dbf86b0c03d1a00c24513541a83f
Patricia Putriningsih Pesahnia Djami <patricia.djami@macio.de>
2018-10-29 13:02:36 +0100

    this is seriously gonna take forever

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="STUPID VUE"/>
+    <div>
+      Haters gonna hate.\n
+      this fucking vue doesn' work with LESS
+    </div>
   </div>
 </template>
 
@@ -17,12 +20,29 @@ export default {
 </script>
 
 <style>
+  .animate-box{
+    align-content: center;
+
+    margin-left: 693px;
+
+    margin-top: 100px;
+
+    background-color: #2c0505;
+
+    color: #ffa138;
+
+    margin-right: 660px;
+  }
+
+  body{
+    background-color: #0d1e06;
+  }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #e8b910;
   margin-top: 60px;
 }
 </style>
