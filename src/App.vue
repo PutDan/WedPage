c1e74be7f157b4aa3477b4f0387f8cb60bbf89bb
Patricia Putriningsih Pesahnia Djami <patricia.djami@macio.de>
2018-10-29 13:43:27 +0100

    frontend sucks

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,13 @@
   <div id="app">
     <HelloWorld msg="STUPID VUE"/>
     <div>
-      Haters gonna hate.\n
-      this fucking vue doesn' work with LESS
+      Haters gonna hate.
     </div>
+    <div>this fucking vue doesn' work with LESS.</div>
+    <div>Vue is definitely made for stupid UI designers that doesn't know how to code</div>
+    <div>VUE i awful, I don't want to work with VuE anymore</div>
+    <div>there are shitty examples and unclear tutorials. IT takes FUCKING 60 mins to understand this FUCKING PIECE OF MESSY STRUCTURE OF FUCKING FRONTEND CODES</div>
+  <h1> just stop using VUE, this is a waste of time.</h1>
   </div>
 </template>
 
@@ -22,15 +26,10 @@ export default {
 <style>
   .animate-box{
     align-content: center;
-
     margin-left: 693px;
-
     margin-top: 100px;
-
     background-color: #2c0505;
-
     color: #ffa138;
-
     margin-right: 660px;
   }
 
