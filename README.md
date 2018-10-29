bb564b02a94fa4ba42fc1f8b7609668ea6f062ba
Patricia Putriningsih Pesahnia Djami <patricia.djami@macio.de>
2018-10-29 14:51:42 +0100

    fixed the architecture

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#HOW-TOs
+# untitled
 
 ## Project setup
 ```
@@ -23,4 +23,14 @@ npm run test
 ### Lints and fixes files
 ```
 npm run lint
+```
+
+### Run your end-to-end tests
+```
+npm run test:e2e
+```
+
+### Run your unit tests
+```
+npm run test:unit
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,29 @@
 <template>
   <div id="app">
-    <HelloWorld msg="STUPID VUE"/>
-    <div>
-      Haters gonna hate.
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
-    <div>this fucking vue doesn' work with LESS.</div>
-    <div>Vue is definitely made for stupid UI designers that doesn't know how to code</div>
-    <div>VUE i awful, I don't want to work with VuE anymore</div>
-    <div>there are shitty examples and unclear tutorials. IT takes FUCKING 60 mins to understand this FUCKING PIECE OF MESSY STRUCTURE OF FUCKING FRONTEND CODES</div>
-  <h1> just stop using VUE, this is a waste of time.</h1>
-    <h3>VUE SUX, UI designers are stupid, cannot code</h3>
-    <div>VUE Tutorials are made from nerds for nerds. They are gonna make a perfect profs at universities, bc they talk to themselves</div>
-    <div> and also </div>
-    <div> I don't see how this is good. It takes way too much time to learn, companies are gonna hate this.</div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-  .animate-box{
-    align-content: center;
-    margin-left: 693px;
-    margin-top: 100px;
-    background-color: #2c0505;
-    color: #ffa138;
-    margin-right: 660px;
-  }
-
-  body{
-    background-color: #0d1e06;
-  }
+<style lang="less">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #e8b910;
-  margin-top: 60px;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
